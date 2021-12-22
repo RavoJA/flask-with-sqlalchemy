77fbf87e4e1f04ecf8f36a632702146847df79a1
Jean Aimé <jeanaime.ravomanana@gmail.com>
2021-12-22 12:22:18 +0300

    add database connexion

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,17 @@
+#import os
+#import logging
+#logging.warn(os.environ["DUMMY"])
+
+# wsgi.py
+# pylint: disable=missing-docstring
+
 from flask import Flask
+from config import Config
 app = Flask(__name__)
+app.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
 
 @app.route('/hello', methods=['GET'])
 def hello():
